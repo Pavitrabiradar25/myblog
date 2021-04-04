797098d7232b55e1c58c6c79bd61a395c59d04d4
Pavitra biradar <pavitrabiradar25@gmail.com>
2021-04-04 15:14:23 +0530

    firstcommit

---
+++ blogproject/blogapp/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from blogapp.models import Post
+from blogapp.models import Comment
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display=['title','slug','author','body','publish','created','updated','status']
+	list_filter=('status','author','created','publish',)
+	search_fields=('title','body')
+	raw_id_fields=('author',)
+	date_hierarchy='publish'
+	prepopulated_fields={'slug':('title',)}
+	ordering=['status','publish']
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display=['name','email','post','body','created','updated','active']
+	list_filter=('active','updated','created')
+	search_fields=('name','email','body')
+
+
+admin.site.register(Post,PostAdmin)
